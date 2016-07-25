16e2d2d62472b72f487f3cc5adc6b751d15f077f
Ivan <ivankacir@gmail.com>
2016-07-25 13:53:20 +0700

    Added cat 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var catOneImage = document.getElementById('cat-one');
-
 var catOneClickCount = 0;
-
 catOneImage.addEventListener('click', function(){
     document.getElementById('cat-one-count').innerHTML = ++catOneClickCount;
+}, false);
+
+var catTwoImage = document.getElementById('cat-two');
+var catTwoClickCount = 0;
+catTwoImage.addEventListener('click', function(){
+    document.getElementById('cat-two-count').innerHTML = ++catTwoClickCount;
 }, false);
 
 var names = ['Fluffy', 'Chewie'];
